8043760e70e7783babda343adb8c574d6924da3c
A. Svensson <lmasvensson@gmail.com>
2015-04-13 17:29:12 +0200

    Oh yeah, pesky float variables.

--- opensimplex_test.py
+++ opensimplex_test.py
@@ -5,7 +5,7 @@ from opensimplex import OpenSimplexNoise
 
 WIDTH = 512
 HEIGHT = 512
-FEATURE_SIZE = 24
+FEATURE_SIZE = 24.0
 
 def main():
     simplex = OpenSimplexNoise()
@@ -13,8 +13,7 @@ def main():
 
     for y in range(0, HEIGHT):
         for x in range(0, WIDTH):
-            #value = simplex.noise2d(x / FEATURE_SIZE, y / FEATURE_SIZE)
-            value = simplex.noise2d(x * 0.05, y * 0.05)
+            value = simplex.noise2d(x / FEATURE_SIZE, y / FEATURE_SIZE)
             color = int((value + 1) * 128)
             im.putpixel((x, y), color)
 
