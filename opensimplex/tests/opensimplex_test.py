adb1f6a106ec142d3d788d8af6a3850227a2d1d4
A. Svensson <lmasvensson@gmail.com>
2015-04-18 16:35:53 +0200

    Updated tests with 4D.

--- opensimplex/tests/opensimplex_test.py
+++ opensimplex/tests/opensimplex_test.py
@@ -20,7 +20,7 @@ def main():
             im.putpixel((x, y), color)
     im.save('noise2d.png')
 
-    print 'Generating 2D image of 3D slice...'
+    print 'Generating 2D slice of 3D...'
     im = Image.new('L', (WIDTH, HEIGHT))
     for y in range(0, HEIGHT):
         for x in range(0, WIDTH):
@@ -28,6 +28,15 @@ def main():
             color = int((value + 1) * 128)
             im.putpixel((x, y), color)
     im.save('noise3d.png')
+
+    print 'Generating 2D slice of 4D...'
+    im = Image.new('L', (WIDTH, HEIGHT))
+    for y in range(0, HEIGHT):
+        for x in range(0, WIDTH):
+            value = simplex.noise4d(x / FEATURE_SIZE, y / FEATURE_SIZE, 0.0, 0.0)
+            color = int((value + 1) * 128)
+            im.putpixel((x, y), color)
+    im.save('noise4d.png')
 
 
 
