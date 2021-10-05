f792dde2578975bd2b6dbabfda60272f5afd9c00
Alex <github@larus.se>
2021-10-05 16:05:45 +0200

    Refactors initial version using numpy/numba

--- opensimplex/__init__.py
+++ opensimplex/__init__.py
@@ -1,4 +1,63 @@
 
-__version__ = 0.3
+__author__ = "Alex"
+__version__ = 0.4
 
 from .opensimplex import OpenSimplex
+
+_default = OpenSimplex()
+
+"""
+OpenSimplex n-dimensional gradient noise algorithm,
+based on work by Kurt Spencer.
+"""
+
+
+def seed(seed):
+    """
+    Seeds the underlying permutation array (which produces different outputs),
+    using a 64-bit seed number.
+    """
+    global _default
+    _default = OpenSimplex(seed)
+
+
+def noise2(x, y):
+    """
+    Generate 2D OpenSimplex noise from X,Y coordinates.
+    """
+    return _default.noise2(x, y)
+
+
+def noise2array(x, y):
+    """
+    Same as noise2, but works with numpy arrays for better performance.
+    """
+    return _default.noise2array(x, y)
+
+
+def noise3(x, y, z):
+    """
+    Generate 3D OpenSimplex noise from X,Y,Z coordinates.
+    """
+    return _default.noise3(x, y, z)
+
+
+def noise3array(x, y, z):
+    """
+    Same as noise3, but works with numpy arrays for better performance.
+    """
+    return _default.noise3array(x, y, z)
+
+
+def noise4(x, y, z, w):
+    """
+    Generate 4D OpenSimplex noise from X,Y,Z,W coordinates.
+    """
+    return _default.noise4(x, y, z, w)
+
+
+def noise4array(x, y, z, w):
+    """
+    Same as noise4, but works with numpy arrays for better performance.
+    """
+    return _default.noise4array(x, y, z, w)
---
+++ opensimplex/constants.py
@@ -0,0 +1,66 @@
+
+import numpy as np
+
+# Why 3 (and not just 0 or something)? Well I ran into a bug with
+# "overflowing int" errors while refactoring in numpy and using a
+# nonzero seed value... This is a reminder
+DEFAULT_SEED = 3
+
+# Gradients for 2D. They approximate the directions to the
+# vertices of an octagon from the center.
+GRADIENTS2 = np.array([
+    5, 2, 2, 5,
+    -5, 2, -2, 5,
+    5, -2, 2, -5,
+    -5, -2, -2, -5,
+], dtype=np.int64)
+
+# Gradients for 3D. They approximate the directions to the
+# vertices of a rhombicuboctahedron from the center, skewed so
+# that the triangular and square facets can be inscribed inside
+# circles of the same radius.
+GRADIENTS3 = np.array([
+    -11, 4, 4, -4, 11, 4, -4, 4, 11,
+    11, 4, 4, 4, 11, 4, 4, 4, 11,
+    -11, -4, 4, -4, -11, 4, -4, -4, 11,
+    11, -4, 4, 4, -11, 4, 4, -4, 11,
+    -11, 4, -4, -4, 11, -4, -4, 4, -11,
+    11, 4, -4, 4, 11, -4, 4, 4, -11,
+    -11, -4, -4, -4, -11, -4, -4, -4, -11,
+    11, -4, -4, 4, -11, -4, 4, -4, -11,
+], dtype=np.int64)
+
+# Gradients for 4D. They approximate the directions to the
+# vertices of a disprismatotesseractihexadecachoron from the center,
+# skewed so that the tetrahedral and cubic facets can be inscribed inside
+# spheres of the same radius.
+GRADIENTS4 = np.array([
+    3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3,
+    -3, 1, 1, 1, -1, 3, 1, 1, -1, 1, 3, 1, -1, 1, 1, 3,
+    3, -1, 1, 1, 1, -3, 1, 1, 1, -1, 3, 1, 1, -1, 1, 3,
+    -3, -1, 1, 1, -1, -3, 1, 1, -1, -1, 3, 1, -1, -1, 1, 3,
+    3, 1, -1, 1, 1, 3, -1, 1, 1, 1, -3, 1, 1, 1, -1, 3,
+    -3, 1, -1, 1, -1, 3, -1, 1, -1, 1, -3, 1, -1, 1, -1, 3,
+    3, -1, -1, 1, 1, -3, -1, 1, 1, -1, -3, 1, 1, -1, -1, 3,
+    -3, -1, -1, 1, -1, -3, -1, 1, -1, -1, -3, 1, -1, -1, -1, 3,
+    3, 1, 1, -1, 1, 3, 1, -1, 1, 1, 3, -1, 1, 1, 1, -3,
+    -3, 1, 1, -1, -1, 3, 1, -1, -1, 1, 3, -1, -1, 1, 1, -3,
+    3, -1, 1, -1, 1, -3, 1, -1, 1, -1, 3, -1, 1, -1, 1, -3,
+    -3, -1, 1, -1, -1, -3, 1, -1, -1, -1, 3, -1, -1, -1, 1, -3,
+    3, 1, -1, -1, 1, 3, -1, -1, 1, 1, -3, -1, 1, 1, -1, -3,
+    -3, 1, -1, -1, -1, 3, -1, -1, -1, 1, -3, -1, -1, 1, -1, -3,
+    3, -1, -1, -1, 1, -3, -1, -1, 1, -1, -3, -1, 1, -1, -1, -3,
+    -3, -1, -1, -1, -1, -3, -1, -1, -1, -1, -3, -1, -1, -1, -1, -3,
+], dtype=np.int64)
+
+
+STRETCH_CONSTANT2 = -0.211324865405187    # (1/Math.sqrt(2+1)-1)/2
+SQUISH_CONSTANT2 = 0.366025403784439      # (Math.sqrt(2+1)-1)/2
+STRETCH_CONSTANT3 = -1.0 / 6              # (1/Math.sqrt(3+1)-1)/3
+SQUISH_CONSTANT3 = 1.0 / 3                # (Math.sqrt(3+1)-1)/3
+STRETCH_CONSTANT4 = -0.138196601125011    # (1/Math.sqrt(4+1)-1)/4
+SQUISH_CONSTANT4 = 0.309016994374947      # (Math.sqrt(4+1)-1)/4
+
+NORM_CONSTANT2 = 47
+NORM_CONSTANT3 = 103
+NORM_CONSTANT4 = 30
